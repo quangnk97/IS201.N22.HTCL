6f608767338d8bf6e9896a73ac3c931c35aaeef9
blackie832 <quandang123654@gmail.com>
2023-06-18 11:45:19 +0700

    update function name

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace IS201_N22_HTCL.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap background {
+            get {
+                object obj = ResourceManager.GetObject("background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Blue_logo {
             get {
                 object obj = ResourceManager.GetObject("Blue_logo", resourceCulture);
